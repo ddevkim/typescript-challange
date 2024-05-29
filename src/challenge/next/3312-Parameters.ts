eefd67a804504ec554c028d74e69c1ea992f7241
ddevkim <ddevkim@gmail.com>
2024-05-29 14:37:12 +0900

    returntype

---
+++ src/challenge/next/3312-Parameters.ts
@@ -0,0 +1,53 @@
+/*
+  3312 - Parameters
+  -------
+  by midorizemi (@midorizemi) #easy #infer #tuple #built-in
+
+  ### Question
+
+  Implement the built-in Parameters<T> generic without using it.
+
+  For example:
+
+  ```ts
+  const foo = (arg1: string, arg2: number): void => {}
+
+  type FunctionParamsType = MyParameters<typeof foo> // [arg1: string, arg2: number]
+  ```
+
+  > View on GitHub: https://tsch.js.org/3312
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyParameters<T extends Function> = T extends (...args: infer Params) => any
+  ? Params
+  : void;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+function foo(arg1: string, arg2: number): void {}
+function bar(arg1: boolean, arg2: { a: "A" }): void {}
+function baz(): void {}
+function baf(arg1: string): void {}
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: "A" }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>,
+  Expect<Equal<MyParameters<typeof baf>, [string]>>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/3312/answer
+  > View solutions: https://tsch.js.org/3312/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
+/*
+type MyParameters<T extends Function> = T extends (...args: infer U) => any
+  ? U
+  : never;
+*/
