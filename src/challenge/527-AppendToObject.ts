626178a8fe9dab60530d4788e66f9eb8183097d0
ddevkim <ddevkim@gmail.com>
2024-06-13 18:34:30 +0900

    appendToObject

---
+++ src/challenge/527-AppendToObject.ts
@@ -0,0 +1,96 @@
+/*
+  527 - Append to object
+  -------
+  by Andrey Krasovsky (@bre30kra69cs) #medium #object-keys
+
+  ### Question
+
+  Implement a type that adds a new field to the interface. The type takes the three arguments. The output should be an object with the new field.
+
+  For example
+
+  ```ts
+  type Test = { id: '1' }
+  type Result = AppendToObject<Test, 'value', 4> // expected to be { id: '1', value: 4 }
+  ```
+
+  > View on GitHub: https://tsch.js.org/527
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyRecord<T> = {
+  [K in keyof T]: T[K];
+};
+
+type AppendToObject<O1, NEW_K extends PropertyKey, V> = {
+  [K in keyof O1 | NEW_K]: K extends keyof O1 ? O1[K] : V;
+};
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<AppendToObject<test1, "home", boolean>, testExpect1>>,
+  Expect<Equal<AppendToObject<test2, "home", 1>, testExpect2>>,
+  Expect<Equal<AppendToObject<test3, "moon", false | undefined>, testExpect3>>,
+];
+
+type test1 = {
+  key: "cat";
+  value: "green";
+};
+
+type testExpect1 = {
+  key: "cat";
+  value: "green";
+  home: boolean;
+};
+
+type test2 = {
+  key: "dog" | undefined;
+  value: "white";
+  sun: true;
+};
+
+type t1 = {
+  key: "cat";
+  value: "green";
+};
+
+type t2 = {
+  sun: true;
+};
+
+type t12 = {
+  key: "cat";
+  value: "green";
+  sun: true;
+};
+
+type testExpect2 = {
+  key: "dog" | undefined;
+  value: "white";
+  sun: true;
+  home: 1;
+};
+
+type test3 = {
+  key: "cow";
+  value: "yellow";
+  sun: false;
+};
+
+type testExpect3 = {
+  key: "cow";
+  value: "yellow";
+  sun: false;
+  moon: false | undefined;
+};
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/527/answer
+  > View solutions: https://tsch.js.org/527/solutions
+  > More Challenges: https://tsch.js.org
+*/
